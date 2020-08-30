5467cbf2b610db93e87da612070696b6523a9db7
mateshavel <46944008+mateshavel@users.noreply.github.com>
2020-08-30 21:18:14 +0200

    Add files via upload

---
+++ _new_script.js
@@ -0,0 +1,15 @@
+
+// Trigger class name on load
+/*window.onload = function() {
+    document.body.className += ' loaded'
+};*/
+
+// carousel interval
+$('.carousel').carousel({
+    interval: 10000
+});
+
+/*
+{%DisplayIndex==0? "active" : ""%}
+*/
+
